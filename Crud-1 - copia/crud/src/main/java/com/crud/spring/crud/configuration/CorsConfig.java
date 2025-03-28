adbeadeef0eeba223afdf5a2384a286386dd2457
Davis <draghs79@gmail.com>
2025-03-27 18:17:47 -0600

    CorsConfig

---
+++ Crud-1 - copia/crud/src/main/java/com/crud/spring/crud/configuration/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.crud.spring.crud.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                    .allowedOrigins(
+                        "http://localhost:8080",  // URL de desarrollo de Vue
+                        "https://crud-indol-three.vercel.app/"  // URL de producción
+                        
+                    )
+                    .allowedMethods("GET", "POST", "PUT", "DELETE")
+                    .allowedHeaders("*")
+                    .allowCredentials(true);
+            }
+        };
+    }
+}
